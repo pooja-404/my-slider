c7af3d5c5baccb7bfcf1be5d661cc9ffcf9e56bd
pooja-404 <pk3924792@gmail.com>
2024-03-28 11:50:51 +0530

    commit

--- src/components/Myslider.jsx
+++ src/components/Myslider.jsx
@@ -10,27 +10,26 @@ const Myslider = () => {
         <div className='max-w-[900px] px-3 mx-auto pt-24 pb-7 '>
             <Swiper effect={'flip'} grabCursor={true} pagination={false} navigation={true} modules={[EffectFlip, Pagination, Navigation]} className="mySwiper ">
                 <SwiperSlide className='text-center '>
-                    <img className='w-full' src="https://i.ibb.co/Ch9rZCp/img3.jpg" />
+                    <img className='w-full h-[520px]' src="https://i.ibb.co/7QGQnjG/slip5.jpg" />
                 </SwiperSlide>
                 <SwiperSlide className='text-center'>
-                    <img className='w-full' src=" https://i.ibb.co/1TqFxyW/img1.jpg" />
+                    <img className='w-full h-[520px]' src=" https://i.ibb.co/yS98F0j/slop5.jpg" />
                 </SwiperSlide>
                 <SwiperSlide className='text-center'>
-                    <img className='w-full' src="https://i.ibb.co/9hrRP0C/img2.jpg" />
+                    <img className='w-full h-[520px]' src="https://i.ibb.co/hK4pPcn/slib.webp" />
                 </SwiperSlide>
                 <SwiperSlide className='text-center'>
-                    <img className='w-full' src="https://i.ibb.co/Ch9rZCp/img3.jpg" />
+                    <img className='w-full h-[520px]' src="https://i.ibb.co/mHZR9NC/slib2.jpg" />
                 </SwiperSlide>
                 <SwiperSlide className='text-center'>
-                    <img className='w-full' src="https://i.ibb.co/tJJt3mT/img4.jpg" />
+                    <img className='w-full h-[520px]' src="https://i.ibb.co/5v02jqW/slip3.webp" />
                 </SwiperSlide>
                 <SwiperSlide className='text-center'>
-                    <img className='w-full' src="https://i.ibb.co/tJJt3mT/img4.jpg" />
+                    <img className='w-full h-[520px]' src="https://i.ibb.co/n1zk46G/slip4.jpg" />
                 </SwiperSlide>
             </Swiper>
         </div>
     )
 }
-
 export default Myslider
 
